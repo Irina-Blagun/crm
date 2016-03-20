cdf59572e21d1885c4c5f79df03175ef344488af
Irina Blagun <irinablagun@if-you-see-this-then-your-dns-resolver-is-wrong.telecom.by>
2016-03-21 00:53:17 +0300

    Revert "Edit role"
    This reverts commit f6e1213f6ae386ee6c1018fae84fc24ae9424a53.

--- client/templates/roles/roles.js
+++ client/templates/roles/roles.js
@@ -3,9 +3,3 @@ Template.roles.helpers({
         return Roles.find();
     }
 });
-
-Template.roleItem.helpers({
-    systemString: function(){
-        return this.system ? '+' : '-'
-    }
-});
--- client/templates/roles/roles_add.js
+++ client/templates/roles/roles_add.js
@@ -1,22 +1,40 @@
 Template.addRoles.events({
+    //'click #addRoles-button': function(e, t){
+	//
+    //    function flags() {
+    //        var flags = 0;
+    //        var cbx = document.getElementById("flags").getElementsByTagName("input");
+    //        for (i=0; i < cbx.length; i++) {
+    //            if (cbx[i].type == "checkbox" && cbx[i].checked) {
+    //                flags +=+ cbx[i].value;
+    //            }
+    //        }
+    //        return flags;
+    //    }
+	//
+    //    var role = {
+    //        rolename : t.find('#rolename').value,
+    //        flag : flags()
+    //    }
+	//
+    //    Meteor.call('addRole', role, function(){
+    //        $('.addRoles-form')[0].reset();
+    //    })
+    //}
 	'submit #addRoles-form': function(event, template){
 		event.preventDefault();
 
 		var role = {
 			rolename: template.find('#rolename').value,
-			system: template.find('input[name=system]').checked,
-			flags: 0
+			flag: 0
 		};
 
 		template.findAll('input[name=flag]').forEach(function(flag){
 			if(flag.checked){
-				role.flags += Number(flag.value);
+				role.flag += Number(flag.value);
 			}
 		});
 
-		Meteor.call('addRole', role, function(){
-			event.target.reset();
-		})
-
+		console.log(role);
 	}
 });
