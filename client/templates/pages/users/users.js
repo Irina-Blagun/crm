34a7114ed748ca6edb9d1331e1b676301b2b39a4
IlyaSuhodolskiy <ilya.sukhodolskiy@gmail.com>
2016-03-27 17:14:00 +0300

    Fixed

--- client/templates/pages/users/users.js
+++ client/templates/pages/users/users.js
@@ -1,13 +1,15 @@
 Template.users.helpers({
-    users: function(){
-        return Users.find();
-    },
-    settings: function(){
+	settings: function(){
         return {
             collection: Users,
             rowsPerPage: 10,
             showFilter: true,
-            fields: ['_id', 'createdAt']
+            fields: [
+				{ key: 'createdAt', label: 'Регистрация' },
+				{ key: 'profile.first_name', label: 'Имя' },
+				{ key: 'profile.comp_flags', label: 'Права', hidden: !Meteor.userCheckAccess(-1) },
+				{ key: 'profile.last_name', label: 'Фамилия' }
+			]
         };
     }
 });
