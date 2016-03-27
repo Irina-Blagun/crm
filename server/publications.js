024c32a847d31ba916624c657073ebe4a14dc7f4
Irina Blagun <irinablagun@if-you-see-this-then-your-dns-resolver-is-wrong.telecom.by>
2016-03-27 22:33:41 +0300

    Add company, profile, store

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,5 @@
-Meteor.publish('company', function(){
-	return Companies.find({ uid: this.userId });
+Meteor.publish('company', function(cid){
+	return Companies.find({ _id: cid });
 });
 
 Meteor.publish('stores', function(){
@@ -9,4 +9,11 @@ Meteor.publish('stores', function(){
 
 Meteor.publish('users', function(cid){
 	return Users.find({ 'profile.cid': cid });
+});
+
+Meteor.publish('user', function(){
+	if (this.userId){
+		return Meteor.users.find({_id: this.userId},
+			{fields: {'createdAt': 1}});
+	}
 });
