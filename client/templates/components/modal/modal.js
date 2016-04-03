5e9d96b2db49bb0884db47a09cc16a6eff43e05d
Irina Blagun <irinablagun@if-you-see-this-then-your-dns-resolver-is-wrong.telecom.by>
2016-04-03 18:25:04 +0300

    Modal window design

--- client/templates/components/modal/modal.js
+++ client/templates/components/modal/modal.js
@@ -1,5 +1,14 @@
 Template.modal.helpers({
-	activeModal: function() {
+	activeModal: function(){
 		return Session.get('activeModal');
+	}
+});
+
+Template.modal.events({
+	'click .cd-modal': function(event){
+		if( $(event.target).is('.cd-modal__close') || $(event.target).is('.cd-modal') ) {
+			event.preventDefault();
+			Session.set('activeModal', null);
+		}
 	}
 });
